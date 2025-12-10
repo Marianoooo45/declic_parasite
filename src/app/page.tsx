7551026b660bbcea3c5fa623ee658f5a6eea55c1
Aurélien Gouyou <aurelien.gouyou@gmail.com>
2025-12-10 14:55:07 +0100

    correction lien page deratisation rats et souris

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function Home() {
                 symptom: "Bruits dans les murs",
                 pest: "Rats & Souris Loiret",
                 image: "https://images.unsplash.com/photo-1657656206271-12b0b877a47b?q=80&w=1176&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                link: "/services/deratisation"
+                link: "/services/deratisation-rats-souris"
               },
               {
                 symptom: "Piqûres au réveil",
