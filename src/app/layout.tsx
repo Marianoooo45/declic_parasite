bc598113a519542308869682b90635d8d0c57686
Aurélien Gouyou <aurelien.gouyou@gmail.com>
2026-01-08 00:50:58 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
   keywords: site.keywords.split(", "),
   icons: {
     icon: [
+      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
